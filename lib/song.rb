9ea3ddeb31cf7e27ee0bcaf66ddc5b1d83673b35
Cody Dupuis <codydupuis99@gmail.com>
2020-08-16 06:24:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,21 @@
 class Song 
   attr_accessor :name, :artist, :genre
   @@count = 0
+  @@genres = []
   
-  def initialize(song)
+  def initialize(song, genre)
     @@count+=1
+    @genre = genre
+    @@genre << genre
   end
   
   def count
     @@count
   end
+  
+  def genres
+    
+  end
+  
   
 end
